5c838f048e5da39c230bcf7d872cd840c940182e
Mahendra Pahariya <mpahariya@yahoo.com>
2020-05-04 00:16:45 -0400

    Revert "Added Code to TestClass.java"
    This reverts commit a5d6d697612d1d3af0125f91400efdc5b71baa0d.

--- TestProject/src/testpackage/TestClass.java
+++ TestProject/src/testpackage/TestClass.java
@@ -1,14 +1,5 @@
 package testpackage;
 
 public class TestClass {
-	
-	/**
-	 * @author Mahendra
-	 */
-	
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!");
-	}
 
 }
